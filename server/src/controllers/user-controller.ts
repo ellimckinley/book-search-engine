bf38a63c58470e32b62b5c246c108f7cc3c765a0
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-05-01 20:36:38 -0600

    removed commented out code and refined auth and server files

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,13 +1,14 @@
 import type { Request, Response } from 'express';
-// import user model
 import User from '../models/User.js';
-// import sign token function from auth
 import { signToken } from '../services/auth.js';
 
-// get a single user by either their id or their username
+// Get a single user by ID or username
 export const getSingleUser = async (req: Request, res: Response) => {
   const foundUser = await User.findOne({
-    $or: [{ _id: req.user ? req.user._id : req.params.id }, { username: req.params.username }],
+    $or: [
+      { _id: req.user ? req.user._id : req.params.id },
+      { username: req.params.username }
+    ],
   });
 
   if (!foundUser) {
@@ -17,21 +18,37 @@ export const getSingleUser = async (req: Request, res: Response) => {
   return res.json(foundUser);
 };
 
-// create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+// Create a user, sign a token, and send it back
 export const createUser = async (req: Request, res: Response) => {
-  const user = await User.create(req.body);
+  try {
+    const user = await User.create(req.body);
 
-  if (!user) {
-    return res.status(400).json({ message: 'Something is wrong!' });
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+
+    const token = signToken({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+
+    return res.json({ token, user });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Server error' });
   }
-  const token = signToken(user.username, user.password, user._id);
-  return res.json({ token, user });
 };
 
-// login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-// {body} is destructured req.body
+// Login a user, sign a token, and send it back
 export const login = async (req: Request, res: Response) => {
-  const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+  const user = await User.findOne({
+    $or: [
+      { username: req.body.username },
+      { email: req.body.email }
+    ]
+  });
+
   if (!user) {
     return res.status(400).json({ message: "Can't find this user" });
   }
@@ -41,12 +58,17 @@ export const login = async (req: Request, res: Response) => {
   if (!correctPw) {
     return res.status(400).json({ message: 'Wrong password!' });
   }
-  const token = signToken(user.username, user.password, user._id);
+
+  const token = signToken({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  });
+
   return res.json({ token, user });
 };
 
-// save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-// user comes from `req.user` created in the auth middleware function
+// Save a book to user's savedBooks
 export const saveBook = async (req: Request, res: Response) => {
   try {
     const updatedUser = await User.findOneAndUpdate(
@@ -61,15 +83,17 @@ export const saveBook = async (req: Request, res: Response) => {
   }
 };
 
-// remove a book from `savedBooks`
+// Remove a book from savedBooks
 export const deleteBook = async (req: Request, res: Response) => {
   const updatedUser = await User.findOneAndUpdate(
     { _id: req.user._id },
     { $pull: { savedBooks: { bookId: req.params.bookId } } },
     { new: true }
   );
+
   if (!updatedUser) {
     return res.status(404).json({ message: "Couldn't find user with this id!" });
   }
+
   return res.json(updatedUser);
 };
